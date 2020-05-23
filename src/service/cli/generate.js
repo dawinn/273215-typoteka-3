ab7fed58374535c87075bc44ff2864473edde81c
o.karpik <o.karpik@bars-open.ru>
2020-05-23 13:57:41 +0700

    module4-task1

--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const {nanoid} = require(`nanoid`);
 const {
   getRandomInt,
   shuffle,
@@ -11,8 +12,10 @@ const chalk = require(`chalk`);
 const fs = require(`fs`).promises;
 
 const {
-  ExitCode
-} = require(`../constants`);
+  ExitCode,
+  MAX_ID_LENGTH,
+  MAX_COMMENTS,
+} = require(`../../constants`);
 
 const DEFAULT_COUNT = 1;
 const COUNT_MAX = 1000;
@@ -21,6 +24,7 @@ const FILE_NAME = `mocks.json`;
 const FILE_SENTENCES_PATH = `./data/sentences.txt`;
 const FILE_TITLES_PATH = `./data/titles.txt`;
 const FILE_CATEGORIES_PATH = `./data/categories.txt`;
+const FILE_COMMENTS_PATH = `./data/comments.txt`;
 
 const readContent = async (filePath) => {
   try {
@@ -32,18 +36,29 @@ const readContent = async (filePath) => {
   }
 };
 
-const generatePublications = (count, titles, categories, sentences) => {
+const generateComments = (count, comments) => (
+  Array(count).fill({}).map(() => ({
+    id: nanoid(MAX_ID_LENGTH),
+    text: shuffle(comments)
+    .slice(0, getRandomInt(1, 3))
+    .join(` `),
+  }))
+);
+
+const generatePublications = (count, titles, categories, sentences, comments) => {
   const currentDate = new Date();
   const threeMonthAgo = new Date(currentDate.getFullYear(), currentDate.getMonth() - 3, currentDate.getDate());
   return Array(count).fill({}).map(() => {
       const randomDate = dateFormat((getRandomInt(threeMonthAgo.getTime(), currentDate.getTime())), `%Y-%m-%d %H:%M:%S`);
 
       return {
+        id: nanoid(MAX_ID_LENGTH),
         title: titles[getRandomInt(0, titles.length - 1)],
+        comments: generateComments(getRandomInt(1, MAX_COMMENTS), comments),
         announce: shuffle(sentences).slice(0, 5).join(` `),
         fullText: shuffle(sentences).slice(0, getRandomInt(0, sentences.length - 1)).join(` `),
         createdDate: randomDate,
-        category: shuffle(categories).slice(0, getRandomInt(0, categories.length - 1)),
+        category: [categories[getRandomInt(0, categories.length - 1)]],
       };
   });
 };
@@ -54,6 +69,7 @@ module.exports = {
     const sentences = await readContent(FILE_SENTENCES_PATH);
     const titles = await readContent(FILE_TITLES_PATH);
     const categories = await readContent(FILE_CATEGORIES_PATH);
+    const comments = await readContent(FILE_COMMENTS_PATH);
     const [count] = args;
     const countPublication = Number.parseInt(count, 10) || DEFAULT_COUNT;
 
@@ -62,7 +78,7 @@ module.exports = {
       return true;
     }
 
-    const content = JSON.stringify(generatePublications(countPublication, titles, categories, sentences));
+    const content = JSON.stringify(generatePublications(countPublication, titles, categories, sentences, comments));
 
     try {
       await fs.writeFile(FILE_NAME, content);
